d2ecb91503cdb22e80f5f7fa8462f9fe49dba951
MrKirill0325 <78133560+MrKirill0325@users.noreply.github.com>
2021-02-01 19:47:48 +0300

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,12 @@
+
+public class Strings {
+
+   public static void main(String[] args) {
+      //String относится к ссылочным типам данных и является классом, поэтому при декларировании пишется с большой буквы. 
+      String a = "Hello";
+      String b = " ";
+      String c = "world!";
+      System.out.println(a + b + c);
+   }
+
+}
